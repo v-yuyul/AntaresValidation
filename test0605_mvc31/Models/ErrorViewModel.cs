110beb29f992ff53e4186b0c24c39ca8c32f8ce3
Cindy Liu (CSI Interfusion Inc) <v-yuyul@microsoft.com>
2020-06-05 17:00:59 +0800

    update mvc31 project

---
+++ test0605_mvc31/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test0605_mvc31.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
